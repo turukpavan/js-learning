bf43720be21fe82feea3101430f45e15fb48b190
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-08 12:53:12 +0530

    js program for swapping
    js program for swapping of two numbers

---
+++ 01_swapVariables.js
@@ -0,0 +1,12 @@
+console.log("WAP to swap variable values using third variable");
+var n1=100;
+var n2=200;
+console.log("=============Before Swap=============");
+console.log("n1:",n1,"n2:",n2);
+
+console.log("=============After Swap=============");
+var temp=n1;
+n1=n2;
+n2=temp;
+console.log("n1:",n1,"n2:",n2);
+
