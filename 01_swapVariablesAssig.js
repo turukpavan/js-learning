1a79e699bcaf44a85f00c60e4d0381a8012de5b7
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-08 12:43:39 +0530

    assignment 2
    assignment 2 for js swapping of values

---
+++ 01_swapVariablesAssig.js
@@ -0,0 +1,24 @@
+console.log("===========step 1: swapping of two variables=============");
+var sweety="Sweety";
+var cutie="cutie";
+console.log("====Before Swap====");
+console.log("sweety:",sweety,"cutie:",cutie);
+
+console.log("====After Swap====");
+var temp=sweety;
+sweety=cutie;
+cutie=temp;
+console.log("sweety:",sweety,",cutie:",cutie);
+
+console.log("===========step 3: swapping of three variables=============");
+var num1=100;
+var num2=200;
+var num3=300;
+console.log("====Before Swap====");
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
+console.log("====After Swap====");
+var  temp=num1;
+num1=num2;
+num2=num3;
+num3=temp;
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
