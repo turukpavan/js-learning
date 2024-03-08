2746ca0aaf91985c3c78a139f075e6cf92e4a04e
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-08 12:42:39 +0530

    assignment1
    assignment1 for js variable

---
+++ 01_variableAssign.js
@@ -0,0 +1,34 @@
+console.log("============step 1================");
+
+var myFullName; //variable Declaration 
+console.log("my Full Name value before initialization :",myFullName);
+myFullName="Pavan Turuk" // variable Initialization
+console.log("my Full Name value after initialization :",myFullName);
+myFullName="Pavan Dilip Turuk" // variable updation
+console.log("my Full Name value after updation :",myFullName);
+
+console.log("============step 2================");
+var myLove="i love only JS";
+console.log("Value of mylove :",myLove);
+
+console.log("============step 3================");
+var firstName="Pavan";
+console.log("First Name :",firstName);
+
+var lastName="Turuk";
+console.log("Last Name :",lastName);
+
+var age=23;
+console.log("Age :",age);
+
+var address="Murtizapur";
+console.log("Address :",address);
+
+var pinCode=444107;
+console.log("Pincode :",pinCode);
+
+var gender="male";
+console.log("Gender :",gender);
+
+var isMarried=false;
+console.log("isMarried :",isMarried);
