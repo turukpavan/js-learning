3c90a0c4a02af78fb485bb9cf0b612f90d25098a
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-13 00:16:19 +0530

    string assignment01

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,12 @@
+function stringBasics(){
+    console.log(`==========STEP 1============`);    
+    console.log(`My dream company is "Tata Consultancy Services (TCS)"`)
+    console.log(`==========STEP 2============`);    
+
+    var hobby1='To play cricket';
+    var hobby2='tinkerers'
+    var hobby3='reading stories'
+    console.log(`My hobbies are :1.${hobby1} 2.${hobby2} 3.${hobby3}`);
+    console.log(`Sum of total number of characters in all 3 hobbies :${hobby1.length+hobby2.length+hobby3.length}`);
+}
+stringBasics();
