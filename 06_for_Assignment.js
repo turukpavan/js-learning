969040a456a6d5c41391c5fb87a34ffe4be5c347
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-19 17:50:04 +0530

    06_for_Assignment

---
+++ 06_for_Assignment.js
@@ -0,0 +1,29 @@
+console.log(`1. WAP to print numbers from 5 to 15 incrementing by 1`);
+for (let index = 5; index <=15; index++) {
+    console.log(index);
+}
+
+console.log(`2. WAP to print numbers from 50 to 40 by decrementing by 1`);
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+console.log(`3. WAP to find first 15 odd numbers`);
+for (let index = 1; index < 30; index=index+2) {
+    console.log(index);
+}
+console.log(`4. WAP to find first 10 even numbers`);
+for (let index = 0; index <20; index=index+2) {
+    console.log(index);
+}
+console.log(`5. WAP to print table of 5 like -> 5 10 15 20 25.......50`);
+for (let index = 1; index <= 10; index++) {
+    console.log(5*index);
+}
+console.log(`6. WAP to print table of 10 like -> 10 20 30 ...100`);
+for (let index = 1; index <= 10; index++) {
+    console.log(10*index);
+}
+console.log(`7. WAP to print table of 10 in reverse order like -> 100 90 80 ...10`);
+for (let index = 10; index >= 1; index--) {
+    console.log(10*index);
+}
