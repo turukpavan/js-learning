b052743eaa9bc95283e4c0fd28319624f94321bc
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-19 17:55:54 +0530

    operators file

---
+++ 05_incrementOperator.js
@@ -0,0 +1,12 @@
+var n1=5;
+var result= n1++ + ++n1 + n1++;
+console.log(n1); 
+console.log(result);
+var a=2;
+var b = ++a + ++a + a++ - --a;
+console.log(a);
+console.log(b);
+var a=4;
+var b=a-- + ++a + ++a + a++
+console.log(a);
+console.log(b);
