87cb46d5b10e1c4d1e90a037cf1824744bdc9f31
PavanTuruk <153252840+PavanTuruk@users.noreply.github.com>
2024-03-26 19:09:56 +0530

    06_factorialAssig2

---
+++ 06_factorial_A.js
@@ -0,0 +1,18 @@
+function factorialOfNum(n) {
+    let fact=1;
+    if (typeof n=='string' || n==NaN || n==undefined) {
+        console.log(`INVALID INPUT`);
+    }else{
+        for (let i = 1; i <= n; i++) {
+            fact=fact*i;
+        }
+        console.log(`Factorial of ${n} : ${fact}`);
+    }
+}
+
+factorialOfNum(5);
+factorialOfNum(3);
+factorialOfNum(null);
+factorialOfNum(8);
+factorialOfNum(undefined);
+factorialOfNum(9);
